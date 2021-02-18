b959ca84f0cc60cb1d245b73376d93c5bcb6b69a
john <john-a-harper@hotmail.com>
2021-02-18 10:04:45 -0800

    Yet more "functionalizing"

--- src/main/kotlin/ClassMetadata.kt
+++ src/main/kotlin/ClassMetadata.kt
@@ -7,22 +7,16 @@ data class ClassMetadata(
         (jsonMetadata["metadata"]
         ?.get("collection")
         ?.asArray()
-        ?.filter{it?.get("name")!=null}
-        ?.map{Pair(it["name"]!!.asString()!!,
-            AttributeMetadata(it["name"]!!.asString(), this, it!!))} ?: listOf())
+        ?.filter{ it.get("name") !=null }
+        ?.map{Pair(
+            it["name"]!!.asString(),
+            AttributeMetadata(it["name"]!!.asString(), this, it))} ?: listOf())
         .toMap()
     val attributes get() = attributeMap.values
-    val collections: MutableList<AttributeMetadata> =
-        attributeMap.values.filter{it.isCollection}.toMutableList()
+    val collections = attributeMap.values.filter{it.isCollection}.toList()
     var container: AttributeMetadata? = null
                             private set
-    //val container: AttributeMetadata get() { return _container!! }
     val parentClass: ClassMetadata? get() { return container?.myClass }
 
     fun getAttribute(aname: String) = attributeMap[aname]
-    //fun getCollections(): List<AttributeMetadata> = collections
-
-    init {
-   }
-
 }
--- src/main/kotlin/Metadata.kt
+++ src/main/kotlin/Metadata.kt
@@ -1,27 +1,19 @@
 class Metadata (
-    do_load: Boolean = true
 ) {
-    private var classes: MutableMap<String, ClassMetadata> = mutableMapOf()
-
-    fun load() {
-        val json = Rest.get("rest/top/metadata/", mapOf("level" to "full"))
-        val classMd = json?.get("metadata")?.get("collection")
-        if (classMd != null) {
-            for (c in classMd.asArray()) {
-                val className = c["name"]?.asString() ?: ""
-                classes[className] = ClassMetadata(className, c)
+    val classes: Map<String, ClassMetadata> =
+        Rest.get("rest/top/metadata/", mapOf("level" to "full"))
+            ?.get("metadata")
+            ?.get("collection")
+            ?.asArray()
+            ?.map{
+                Pair(it["name"]!!.asString(),
+                    ClassMetadata(it["name"]!!.asString(), it))
             }
-        }
-    }
+            ?.toMap() ?: mapOf()
 
     fun getClass(className: String) = classes[className]
     fun getAttribute(className: String, aname: String) = getClass(className)?.getAttribute(aname)
 
-    init {
-        if (do_load) {
-            load()
-        }
-    }
     companion object {
         private var theMetadata: Metadata? = null
         fun load() = Metadata().also { theMetadata = it }
